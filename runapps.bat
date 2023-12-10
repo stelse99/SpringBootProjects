5b8daa6de55acf9cb9ab5ca4f1f9425c93386599
stelse99 <stelse99@gmail.com>
2023-12-10 18:26:07 +0900

    runapps.bat again

--- runapps.bat
+++ runapps.bat
@@ -1,13 +1,13 @@
 docker rm -f mysqldb
-timeout /t 10 /nobreak > NUL
+timeout /t 5 /nobreak > NUL
 docker rm -f app
-timeout /t 10 /nobreak > NUL
-docker run -d -p 3307:3306 --name mysqldb --env-file mysqlenv mysql
-timeout /t 10 /nobreak > NUL
+timeout /t 5 /nobreak > NUL
+docker run -d -p 3307:3306 --name mysqldb --env-file mysqlenv  -v "C:\Users\stels\db":/var/lib/mysql mysql
+timeout /t 5 /nobreak > NUL
 docker network connect spring-network mysqldb
-timeout /t 10 /nobreak > NUL
+timeout /t 5 /nobreak > NUL
 docker run -d -p 9090:8080 --name app --net spring-network --env-file appenv app
-timeout /t 10 /nobreak > NUL
+timeout /t 5 /nobreak > NUL
 docker logs mysqldb
-timeout /t 10 /nobreak > NUL
+timeout /t 5 /nobreak > NUL
 docker logs app
